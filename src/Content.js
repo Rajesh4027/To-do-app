8cdb457d9d1704b50c260a5c9b3613f09984fb0c
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-03-20 19:44:09 +0530

    increment added

--- src/Content.js
+++ src/Content.js
@@ -8,24 +8,31 @@ const Content = () => {
     const int = Math.floor(Math.random()*3);
     return name[int];
   }
-  // const handleClick1 = (e) => {
-  //     console.log(e.target.innerText);
-  // }
-  // const handleClick = () => {
-  //   const name1 = window.prompt("What is your name");
-  //   console.log(`Your name is ${name1}`)
-  // }
+  const handleClick1 = (e) => {
+      console.log(e.target.innerText);
+  }
+  const handleClick = () => {
+    const name1 = window.prompt("What is your name");
+    console.log(`Your name is ${name1}`)
+  }
   const [count, setCount] = useState(99);
+
+  function incrementFunction() {
+    setCount(count + 1);
+  }
+  function decrementFunction() {
+    setCount(count -1);
+  }
 
     return(
       <main>
-           {/* <p onDoubleClick={() => handleClick("bala")}>Let's {handleName()} money</p> */}
-           {/* <button onClick={(e) => handleClick1(e)}>Subscribe</button> */}
+           <p onDoubleClick={() => handleClick("bala")}>Let's {handleName()} money</p>
+          <button onClick={(e) => handleClick1(e)}>Subscribe</button>
            <p >Let's  money</p>
-           <button>Subscribe</button><br />
-           <button>-</button><br />
+           <button>Subscribe my channel</button><br />
+           <button onClick={decrementFunction}>-</button><br />
            <span>{count}</span><br />
-           <button>+</button>
+           <button onClick={incrementFunction}>+</button>
       </main>
     )
 }
